d31fcec98afc027bca5a3be251169362c9be50ce
Henry <141684228+henry2craftman@users.noreply.github.com>
2024-04-19 12:35:13 +0900

    feat: 1. 코딩규칙 연습

--- 240419/240419/Program.cs
+++ 240419/240419/Program.cs
@@ -1,4 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// 1. 코딩규칙 연습
+// 내용
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!"); // 한줄 주석: Ctrl + K + C
 Console.WriteLine("Hello, World!"); // 한줄 주석 해제: Ctrl + K + U
 Console.WriteLine("Hello, World!"); // 영역 주석 & 주석 해제: Ctrl + Shift + /
--- 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
+++ 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240419")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db818d2979137f2f3913f822385f0a891d669d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("240419")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240419")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
