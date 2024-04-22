228754e06a4c8e71862b0d5c7afd83d238093c92
Henry <141684228+henry2craftman@users.noreply.github.com>
2024-04-22 17:58:16 +0900

    feat: 7. 시간지연 및 도서관리시스템 실습

--- 240419/240419/Program.cs
+++ 240419/240419/Program.cs
@@ -1,66 +1,86 @@
-﻿// 6. IF, Switch 조건문
+﻿// 7. 시간지연
 
-bool isActive = true;
+Console.WriteLine("시작");
 
-if(isActive)
+Thread.Sleep(3000);
+
+Console.WriteLine("종료");
+
+// 실습
+// 도서관리 시스템
+// 기능1. 도서등록 -> "퓨처셀프", "3,1" 입력 -> Dictionary<string, string> library에 저장
+// 기능2. 도서검색 -> "퓨처셀프" 검색 -> 위치(3,1) 출력 -> library사용 검색
+// 기능3. 회원등록 -> "회원명", "회원번호(임의의 번호)"  -> Dictionary<string, int> members에 저장
+// 기능4. 예약대출 -> 도서검색(빌려간 책)인 경우 "이름","책이름" Queue에 저장 -> Queue<string> waitings
+
+// 입출력예시
+// 번호를 선택해 주세요.(0: 도서등록, 1: 도서검색, 2: 회원등록, 3: 대출예약)
+// 0
+// 도서명을 입력해 주세요.
+// 퓨처셀프
+// 도서 위치를 입력해 주세요.
+// 3,1
+// 등록이 완료되었습니다. 퓨처셀프(3,1)
+
+// 번호를 선택해 주세요.(0: 도서등록, 1: 도서검색, 2: 회원등록, 3: 대출예약)
+// 1
+// 도서 명 또는 도서 번호를 입력해 주세요.
+// 퓨처셀프
+// 퓨처셀프는 3,1에 있습니다.
+
+// 번호를 선택해 주세요.(0: 도서등록, 1: 도서검색, 2: 회원등록, 3: 대출예약)
+// 2
+// 회원등록 페이지 입니다. 회원이름을 입력해 주세요.
+// 신태욱
+// 비밀번호를 입력해 주세요.
+// 1234
+// 회원등록이 완료되었습니다.
+
+// 번호를 선택해 주세요.(0: 도서등록, 1: 도서검색, 2: 회원등록, 3: 대출예약)
+// 3
+// 대출예약을 위한 회원명과 도서명을 입력해 주세요.
+// 신태욱,퓨처셀프
+// 예약되었습니다.
+
+Dictionary<string, string> bookList = new Dictionary<string, string>();
+Dictionary<string, string> memberList = new Dictionary<string, string>();
+
+while (true)
 {
-    Console.WriteLine("작동 중 입니다.");
-}
-else
-{
-    Console.WriteLine("정지상태 입니다.");
-}
-
-int number = 5;
-
-// 비교 연산자
-if(number == 0)      Console.WriteLine("");
-else if(number != 0) Console.WriteLine("");
-else if(number > 0)  Console.WriteLine("");
-else if(number < 0)  Console.WriteLine("");
-else if(number >= 0) Console.WriteLine("");
-else if(number <= 0) Console.WriteLine("");
-
-// 논리 연산자 AND(&&), OR(||), NOT(!)
-if (number == 0 && isActive == true)       Console.WriteLine("");
-else if (number == 0 || isActive == false) Console.WriteLine("");
-else if (!isActive)                        Console.WriteLine("");
-
-// 번호에 따른 시퀀스 작동
-int status = 0;
-
-switch(status)
-{
-    case 0:
-        Console.WriteLine("0번 상태입니다.");
-        break;
-    case 1:
-        Console.WriteLine("1번 상태입니다.");
-        break;
-    case 2:
-        Console.WriteLine("2번 상태입니다.");
-        break;
-}
-
-// 실습3. PLC 5초 딜레이 신호등을 if문 또는 switch case문으로 작성해 봅니다.
-// 조건: status / 황색(0), 적색(1), 녹색(2)
-// M10(0), M11(1), M12(2) -> Console.WriteLine("황색 전구 ON");
-// Timer -> Console.WriteLine("5초 Timer On");
-
-for(int i = 0; i < 3; i++)
-{
-    if(i == 0)
+    Console.WriteLine("번호를 선택해 주세요.(0: 도서등록, 1: 도서검색, 2: 회원등록, 3: 대출예약)");
+    string status = Console.ReadLine();
+    switch(status)
     {
-        Console.WriteLine("황색 전구 ON");
+        case "0":
+            RegisterBook();
+            break;
+        case "1":
+            SearchBook();
+            break;
+        case "2":
+            RegisterMember();
+            break;
+        case "3":
+            MakeReservation();
+            break;
+        default:
+            Console.WriteLine("잘못 입력하였습니다. 다시 입력해 주세요.");
+            break;
     }
-    else if(i == 1)
-    {
-        Console.WriteLine("적색 전구 ON");
-    }
-    else if(i == 2)
-    {
-        Console.WriteLine("녹색 전구 ON");
-    }
+}
 
-    Console.WriteLine("5초 Timer On");
+void MakeReservation()
+{
+}
+
+void RegisterMember()
+{
+}
+
+void SearchBook()
+{
+}
+
+void RegisterBook()
+{
 }
