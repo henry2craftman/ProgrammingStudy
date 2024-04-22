2f061ce6ee6a83bc41395572373143473e8f63d2
Henry <141684228+henry2craftman@users.noreply.github.com>
2024-04-22 16:24:27 +0900

    feat: 6. IF, Switch 조건문 + 실습

--- 240419/240419/Program.cs
+++ 240419/240419/Program.cs
@@ -1,158 +1,66 @@
-﻿// 5. 컬렉션 연습 2
+﻿// 6. IF, Switch 조건문
 
-/* Stack 연습
- * Stack<int> numbers = new Stack<int>();
-numbers.Push(10); // 값을 쌓아 올린다.
-numbers.Push(100);
-numbers.Push(1000);
+bool isActive = true;
 
-foreach (int number in numbers)
+if(isActive)
 {
-    Console.WriteLine(number);
+    Console.WriteLine("작동 중 입니다.");
+}
+else
+{
+    Console.WriteLine("정지상태 입니다.");
 }
 
-Console.WriteLine("------------");
+int number = 5;
 
-int peekNumber = numbers.Peek();
-Console.WriteLine(peekNumber);
+// 비교 연산자
+if(number == 0)      Console.WriteLine("");
+else if(number != 0) Console.WriteLine("");
+else if(number > 0)  Console.WriteLine("");
+else if(number < 0)  Console.WriteLine("");
+else if(number >= 0) Console.WriteLine("");
+else if(number <= 0) Console.WriteLine("");
 
-bool isExist = numbers.Contains(100);
-if (isExist)
-    Console.WriteLine("100이 존재합니다.");
+// 논리 연산자 AND(&&), OR(||), NOT(!)
+if (number == 0 && isActive == true)       Console.WriteLine("");
+else if (number == 0 || isActive == false) Console.WriteLine("");
+else if (!isActive)                        Console.WriteLine("");
 
-int value = numbers.Pop(); // 쌓아 올린 값을 순서대로 꺼낸다.
-Console.WriteLine(value);
+// 번호에 따른 시퀀스 작동
+int status = 0;
 
-value = numbers.Pop(); // 쌓아 올린 값을 순서대로 꺼낸다.
-Console.WriteLine(value);
-
-value = numbers.Pop(); // 쌓아 올린 값을 순서대로 꺼낸다.
-Console.WriteLine(value);*/
-
-/* < 실습 1 - Stack >
-작업(파워포인트)을 되돌리기 상태로 만들어주는 예제
-1. 특정 횟수 만큼 파워포인트 각 작업 반복해서 넣고
-2. (넣는 과정이 끝나면) 1번 입력(되돌리기 버튼)을 누를 경우
-되돌리기 작업(POP)과 현재 진행된 작업의 이름을 Stack에 쌓인 순서대로 출력
-
-Stack<string> jobs = new Stack<string>();
-Console.WriteLine("< 초기 Jobs List >");
-jobs.Push("1. 삽입 메뉴 클릭");
-jobs.Push("2. 표 아이콘 클릭");
-jobs.Push("3. 표 크기 설정 및 표 생성");
-jobs.Push("4. 0,0에 안녕하세요 입력");
-jobs.Push("5. 0,1에 반갑습니다 입력");
-jobs.Push("6. 0,2에 저는 신태욱 입니다 입력");
-
-foreach (string job in jobs)
+switch(status)
 {
-    Console.WriteLine(job);
+    case 0:
+        Console.WriteLine("0번 상태입니다.");
+        break;
+    case 1:
+        Console.WriteLine("1번 상태입니다.");
+        break;
+    case 2:
+        Console.WriteLine("2번 상태입니다.");
+        break;
 }
 
-Console.WriteLine("----------------------------");
+// 실습3. PLC 5초 딜레이 신호등을 if문 또는 switch case문으로 작성해 봅니다.
+// 조건: status / 황색(0), 적색(1), 녹색(2)
+// M10(0), M11(1), M12(2) -> Console.WriteLine("황색 전구 ON");
+// Timer -> Console.WriteLine("5초 Timer On");
 
-for (int i = 0; i < jobs.Count; ) // 0, 1, 2, 3, 4, 5
+for(int i = 0; i < 3; i++)
 {
-    Console.WriteLine("jobs Count: " + jobs.Count);
-    Console.WriteLine("명령을 입력해 주세요. 되돌리기(1)을 입력해 주세요.");
-    string input = Console.ReadLine();
-
-    if(input == "1")
+    if(i == 0)
     {
-        Console.WriteLine("아래 명령을 되돌렸습니다.");
-        Console.WriteLine(jobs.Peek());
-
-        jobs.Pop();
-
-        Console.WriteLine("< Jobs List >");
-
-        foreach (string newJob in jobs)
-        {
-            Console.WriteLine(newJob);
-        }
+        Console.WriteLine("황색 전구 ON");
     }
+    else if(i == 1)
+    {
+        Console.WriteLine("적색 전구 ON");
+    }
+    else if(i == 2)
+    {
+        Console.WriteLine("녹색 전구 ON");
+    }
+
+    Console.WriteLine("5초 Timer On");
 }
-
-
-// 작업 List
-//1. 삽입 메뉴 클릭
-//2. 표 아이콘 클릭
-//3. 표 크기 설정 및 표 생성
-//4. 0,0에 안녕하세요 입력
-//5. 0,1에 반갑습니다 입력
-//6. 0,2에 저는 신태욱 입니다 입력
-
-*/
-
-/* Queue 연습
- * Queue<string> waitingPeople = new Queue<string>();
-waitingPeople.Enqueue("신태욱");
-waitingPeople.Enqueue("김동균");
-waitingPeople.Enqueue("박길수");
-
-foreach(string people in waitingPeople)
-{
-    Console.WriteLine(people);
-}
-
-string name = waitingPeople.Dequeue();
-Console.WriteLine(name);
-
-name = waitingPeople.Dequeue();
-Console.WriteLine(name);
-
-name = waitingPeople.Dequeue();
-Console.WriteLine(name);*/
-
-/* 실습 2 - Queue
- * 도서관 대출자 대기열
- * 1. 대출자 대기열에 맞춰 대출자의 이름과 도서의 이름을 대기열(Queue)에 등록
- * 2. 도서관 직원이 대기열에 맞춰 대출목록에 업데이트
- * 
- * < 입출력 예시 >
- * "대출자 이름을 입력해 주세요."
- * 신태욱
- * "도서 권수를 입력해 주세요."
- * 2
- * "대출을 위한 도서 목록을 입력해 주세요."
- * 퓨처셀프
- * "대출을 위한 도서 목록을 입력해 주세요."
- * 삼국지
- * 
- * Queue에 들어갈 내용 -> 이름: 신태욱\n도서1. 퓨처셀프\n도서2. 삼국지
- * 
- * "도서가 등록되었습니다."
- * 이름: 신태욱
- * 도서1. 퓨처셀프
- * 도서2. 삼국지
- */
-
-Queue<string> libraryQueue = new Queue<string>();
-string totalInfo = "";
-
-Console.WriteLine("대출자 이름을 입력해 주세요.");
-string name = Console.ReadLine();
-Console.WriteLine("도서 권수를 입력해 주세요.");
-string bookNumber = Console.ReadLine();
-int count = int.Parse(bookNumber);
-
-totalInfo = name + "\n" + count + "\n";
-
-for(int i = 0; i < count; i++)
-{
-    Console.WriteLine("대출을 위한 도서 목록을 입력해 주세요.");
-    string bookName = Console.ReadLine();
-    totalInfo += bookName + "\n";
-}
-
-Console.WriteLine("입력된 내용은 아래와 같습니다.");
-Console.WriteLine(totalInfo);
-libraryQueue.Enqueue(totalInfo);
-
-// 도서관 직원이 내용 확인 후 시스템에 입력.
-
-// 최종실습 도서관시스템
-// 1. 도서 검색
-// 2. 도서 입력
-// 3. 도서 반납 리스트
-// 4. 대출 리스트
