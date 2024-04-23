9f05f09882df2c091cc832496d7ada2e86520b8b
Henry <141684228+henry2craftman@users.noreply.github.com>
2024-04-23 11:28:45 +0900

    feat: 9. 반복문

--- 240419/240419/Program.cs
+++ 240419/240419/Program.cs
@@ -1,39 +1,110 @@
-﻿// 8. 열거형
-
-// 열거형 포멧 정의
-public enum Option
-{
-    RegisterBook = 5,
-    SearchBook,
-    RegisterMember,
-    SearchMember,
-    MakeReservation
-}
+﻿// 9. 반복문
 
 public class Student
 {
     public static void Main()
     {
-        // 사용을 위해 선언
-        Option op = Option.RegisterBook;
-
-        switch(op)
+        // for 반복문: 10번 반복하는 반복문
+        // 초기화자;    조건;  증감
+        for(int i = 0; i < 10; i++)
         {
-            case Option.RegisterBook:
-                break;
-            case Option.SearchBook:
-                break;
-            case Option.RegisterMember:
-                break;
-            case Option.SearchMember:
-                break;
-            case Option.MakeReservation:
-                break;
+
         }
 
-        if(op == Option.RegisterBook)
+        // foreach 반복문
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        foreach(int number in numbers)
         {
 
+        }
+
+        // while 반복문: 10번 반복하는 반복문
+        int j = 0;
+        while(j < 10)
+        {
+            j++;
+        }
+
+        // do-while 반복문: 최소 한번은 실행되는 반복문
+        int z = 0;
+        do
+        {
+            z++;
+        } while(z < 10);
+
+        // 실습1. 0~10까지 더한 값을 출력하는 프로그램 만들기
+        int sum = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+            //sum = sum + i;
+            sum += i;
+        }
+        Console.WriteLine(sum);
+
+        // 실습2. 배열의 값을 더하는 프로그램 만들기
+        int[] numberArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        sum = 0;
+        foreach (int number in numberArray)
+        {
+            sum += number;
+        }
+        Console.WriteLine(sum);
+        Console.WriteLine("평균: " + (sum / numberArray.Length));
+
+        sum = 0;
+        for (int i = 0; i < numberArray.Length; i++)
+        {
+            sum += numberArray[i];
+        }
+        Console.WriteLine(sum);
+
+        List<int> list = new List<int>() { 3, 5, 3, 8, 5 };
+        list.Add(3);
+        sum = 0;
+        foreach(int number in list)
+        {
+            sum += number;
+        }
+        Console.WriteLine("합계: " + sum + " / 평균: " + (sum / list.Count));
+
+        // 실습3. 이름리스트에서 내 이름이 있는지 확인하고,
+        // 있다면, 찾았습니다 000은 4번째에 있습니다. 없다면, 없습니다. 출력하는 프로그램 만들기
+        List<string> names = new List<string>(){ "000", "신태욱" };
+
+        int g = 0;
+        foreach(string name in names)
+        {
+            if(name == "신태욱")
+            {
+                Console.WriteLine("찾았습니다. 신태욱은 " + g + "번째에 있습니다.");
+            }
+            else
+            {
+                Console.WriteLine("없습니다.");
+            }
+            g++;
+        }
+
+        string input = Console.ReadLine();
+        Console.WriteLine(input + "은 " + names.IndexOf(input) + "번재에 있습니다.");
+
+        // 반복문 안의 반복문
+        // 구구단 예(2~9단, 각 1~9 단계)
+        // 2단, 2x1=2 / 2x2=4 ..... / 2x9=18
+        // 3단, 3x1=3 / 3x3 =9 .... / 3x9=27
+        // ...
+        
+        // 실습4. 구구단 프로그램 만들기
+        // for문 1
+        for(int i = 2; i < 10; i++)
+        {
+            Console.Write(i + "단, ");
+            // for문 2
+            for(int k = 1; k < 10; k++)
+            {
+                Console.Write($"{i}*{k}={i * k} / ");
+            }
+            Console.Write("\n");
         }
     }
 }
--- 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
+++ 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240419")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228754e06a4c8e71862b0d5c7afd83d238093c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e11aef5840ff0832cbaef139e56b843ffb05537")]
 [assembly: System.Reflection.AssemblyProductAttribute("240419")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240419")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
