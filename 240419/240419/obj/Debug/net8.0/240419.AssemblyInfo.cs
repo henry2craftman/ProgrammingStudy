122a03d336f2dfda094d3e4a10a36497700e3fc0
Henry <141684228+henry2craftman@users.noreply.github.com>
2024-04-23 16:01:12 +0900

    feat: 11. 클래스 메서드 연습

--- 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
+++ 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240419")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f05f09882df2c091cc832496d7ada2e86520b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60be13c5a80e02de8e587d7aac8ab7116be891b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("240419")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240419")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
